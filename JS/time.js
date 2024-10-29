14b01082c3f074a719d449d92126f593d1f7fb00
Ruya Bozcan <93367377+ruyabozcan@users.noreply.github.com>
2024-10-29 14:06:36 +0300

    dlsfljd

--- JS/time.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const datetimeElement = document.getElementById('datetime');
-    const updateTime = () => {
-        const now = new Date();
-        datetimeElement.textContent = now.toLocaleString();
-    };
-    setInterval(updateTime, 1000);
-    updateTime();
-});
